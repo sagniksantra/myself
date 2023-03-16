9c97c4030b0ebd8fdf02af46025a1a743b10968a
sagnik <santrasagnik10@gmail.com>
2023-03-16 20:03:29 +0530

    added blank AboutMe

--- src/components/pages/Events.js
+++ src/components/pages/Events.js
@@ -1,6 +1,40 @@
+// import React from 'react';
+// import '../../App.css';
+
+// export default function Events() {
+//   return <h1 className='events'>Developer, Learner, Dreamer </h1>;
+// }
+
 import React from 'react';
 import '../../App.css';
+import { Button } from './../Button';
+import './../HeroSection.css';
 
-export default function Events() {
-  return <h1 className='events'>Events</h1>;
+function Events() {
+  return (
+    <div className='hero-container'>
+      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <h1>Developer, Learner, Dreamer </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac scelerisque sem. </p>
+      <div className='hero-btns'>
+        {/* <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          more 'bout me
+        </Button> */}
+        {/* <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button> */}
+      </div>
+    </div>
+  );
 }
+
+export default Events;
